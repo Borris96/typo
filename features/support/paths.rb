5f54df0e8143ae290cd6f1ecc510d94bb2f4f838
EC2 Default User <ec2-user@ip-172-31-23-120.us-east-2.compute.internal>
2019-03-12 07:05:43 +0000

    Completed on Mar 12th

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,11 @@ module NavigationHelpers
       '/'
     when /^the new article page$/
       '/admin/content/new'
+    
+    when /^the new category page$/
+      '/admin/categories/new'
+    when /^the existing category page$/
+      '/admin/categories/edit'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -15,6 +15,25 @@ describe Admin::CategoriesController do
     get :index
     assert_response :redirect, :action => 'index'
   end
+  
+  # Test if creating a new category works
+  describe "test_new" do
+    before(:each) do
+      get :edit, :id => Factory(:category).id
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should have valid category' do
+      assigns(:category).should_not be_nil
+      assert assigns(:category).valid?
+      assigns(:categories).should_not be_nil
+    end
+  end
 
   describe "test_edit" do
     before(:each) do
